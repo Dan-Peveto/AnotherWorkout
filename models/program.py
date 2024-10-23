c567a96c8f36d61fa48d457b775ba3c9fa25eed1
Daryl <daryliost@github.com>
2024-10-23 14:23:59 -0700

    V1

--- models/program.py
+++ models/program.py
@@ -1,28 +1,42 @@
 class User:
      def __init__(self, programName):
          self.programName = programName
-        
+#define workout exercies, how many calories burned and for what duration using a class, storing it in list_exercises
 class Workout:
-    list_exercises = []
-    def __init__(self, exercise, cal_out, time):
-         self.exercise = exercise
+    def __init__(self, name, cal_out, time):
+         self.name = name
          self.cal_out = cal_out
          self.time = time
 
-    def cal_burned(self, total):
-        self.total = self.cal_out * total
+    #finding total calories burned per minute
+    def cal_burned(self):
+        self.total = self.cal_out * self.time
         print(self.total)
-        
-sprint = Workout(exercise="sprint",cal_out=100,time=1)
-walk = Workout(exercise="walk",cal_out=50,time=2)
 
-def additional_exercise(): 
-    while True:
-        choice = input("please input workout activity:")
-        Workout.list_exercises.append(choice.strip())
-        for item in Workout.list_exercises:
-            print(item,end=" ")        
-        break
+    #assign userinput to belong to class
+    @classmethod
+    def userinput(cls):
+        name = input("Enter exercise name: ")
+        duration = int(input("Enter duration in minutes: "))
+        intensity = input("Enter intensity (low, medium, high): ")
+
+    def display_workout(self):
+        print(f"Exercise name: {self.name}")
+        print(f"Duration: {self.time} minutes")
+        print(f"calories burned {self.total}")
+
+
 
 if __name__ == '__main__':
-    print(f'your exercise is {walk.exercise} and you will burn {walk.cal_out} calories')
+    list_exercises = []
+    while True:
+        name = Workout.userinput()
+        list_exercises.append(name)
+
+        more = input("Would you like to add another exercise? (y/n): ")
+        if more.lower != 'y':
+            break
+
+
+    for ex in list_exercises:
+        ex.display_workout()
